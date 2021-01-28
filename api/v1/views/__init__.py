bdbd44ba39f953032383fb1f384ba7771478ba0a
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-01-28 17:03:17 -0500

    Solve conflict.

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,8 +6,5 @@ from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
-<<<<<<< HEAD
 from api.v1.views.cities import *
-=======
 from api.v1.views.states import *
->>>>>>> ac1f1de30f90f025f559e9d8e01089ecb912830b
